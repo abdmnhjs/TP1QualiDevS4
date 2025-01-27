55fd8d5e1e8791ab7749e6d7dbe32040341ff78a
abd <22403728@me.uvsq.fr>
2025-01-27 11:44:54 +0100

    documentation de la classe Velo

--- src/main/java/Decorateur/Velo.java
+++ src/main/java/Decorateur/Velo.java
@@ -1,12 +1,24 @@
 package Decorateur;
 
+/**
+ * Class that represents a bicycle, which is an additional armament for a sailboat
+ * and decorates the sailboat.
+ */
 public class Velo extends ArmementSupplementaire {
 
-    public Velo(Voilier composantDecorer){
+    /**
+     * Constructor that allows decorating a sailboat with a bicycle
+     * @param composantDecorer The sailboat component to be decorated
+     */
+    public Velo(Voilier composantDecorer) {
         super(composantDecorer);
     }
 
-    public String toString(){
-        return  composantDecorer.toString() + " et c'est un velo" ;
+    /**
+     * Returns a string representation of the decorated sailboat with additional description
+     * @return A string describing the decorated sailboat
+     */
+    public String toString() {
+        return composantDecorer.toString() + " and it's a bicycle";
     }
 }
