259570f6e834c113f0e04d532487915af540c909
abd <22403728@me.uvsq.fr>
2025-01-27 11:41:23 +0100

    documentation de la classe Monocoque

--- src/main/java/Decorateur/Monocoque.java
+++ src/main/java/Decorateur/Monocoque.java
@@ -1,14 +1,24 @@
 package Decorateur;
 
-public class Monocoque extends Voilier{
+/**
+ * Class that represents a monohull sailboat
+ */
+public class Monocoque extends Voilier {
 
-    public Monocoque(String name, SuivreRoute suivreRoute){
+    /**
+     * Constructor that allows creating a monohull sailboat
+     * @param name The name of the sailboat
+     * @param suivreRoute The follow route behavior of the sailboat
+     */
+    public Monocoque(String name, SuivreRoute suivreRoute) {
         super(name, suivreRoute);
-
     }
 
-    public String toString(){
-        return super.toString() +"\n Le type du voilier est: mutlicoque";
-
+    /**
+     * Returns a string representation of the sailboat with additional description
+     * @return A string describing the sailboat type
+     */
+    public String toString() {
+        return super.toString() + "\n The type of sailboat is: monohull";
     }
 }
