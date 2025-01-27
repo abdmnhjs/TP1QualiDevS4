9a5a0cf601509601950cb1ea5b66caf560f054bf
abd <22403728@me.uvsq.fr>
2025-01-27 11:44:25 +0100

    documentation de l'interface SuivreRoute

--- src/main/java/Decorateur/SuivreRoute.java
+++ src/main/java/Decorateur/SuivreRoute.java
@@ -1,10 +1,12 @@
 package Decorateur;
 
-
+/**
+ * Interface that represents the behavior of following a route
+ */
 public interface SuivreRoute {
 
+    /**
+     * Method to follow a route
+     */
     public void suivreRoute();
-
-
-
 }
