daf27d916685e78d99f50612fd45dd66feb9450a
abd <22403728@me.uvsq.fr>
2025-01-27 11:42:20 +0100

    documentation de la classe Multicoque

--- src/main/java/Decorateur/Multicoque.java
+++ src/main/java/Decorateur/Multicoque.java
@@ -1,17 +1,32 @@
 package Decorateur;
 
-public class Multicoque extends Voilier{
+/**
+ * Class that represents a multihull sailboat
+ */
+public class Multicoque extends Voilier {
 
-    public Multicoque(String nom){
+    /**
+     * Constructor that allows creating a multihull sailboat
+     * @param nom The name of the sailboat
+     */
+    public Multicoque(String nom) {
         super(nom);
     }
 
-    public Multicoque(String nom, SuivreRoute suivreRoute){
+    /**
+     * Constructor that allows creating a multihull sailboat
+     * @param nom The name of the sailboat
+     * @param suivreRoute The follow route behavior of the sailboat
+     */
+    public Multicoque(String nom, SuivreRoute suivreRoute) {
         super(nom, suivreRoute);
     }
 
-    public String toString(){
-        return super.toString() +"\n Le type du voilier est multicoque";
-
+    /**
+     * Returns a string representation of the sailboat with additional description
+     * @return A string describing the sailboat type
+     */
+    public String toString() {
+        return super.toString() + "\n The type of sailboat is: multihull";
     }
 }
